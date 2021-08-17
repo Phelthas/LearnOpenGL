80b38d5f26fb928adbc51ca9d446f678d77b66b5
billthaslu <billthaslu@tencent.com>
2021-08-17 11:11:25 +0800

    fix

--- DemoUtils/DemoUtils/DemoGLGeometry.m
+++ DemoUtils/DemoUtils/DemoGLGeometry.m
@@ -28,22 +28,22 @@ CGFloat topYForSamplingSize(CGSize samplingSize) {
 @implementation DemoGLGeometry
 
 + (GLKVector2)topLeftForSamplingSize:(CGSize)samplingSize {
-    NSAssert((samplingSize.width <= 1 || samplingSize.height <= 1 ), @"samplingSize should be less than 1.0");
+    NSAssert((samplingSize.width <= 1 && samplingSize.height <= 1 ), @"samplingSize should be less than 1.0");
     return GLKVector2Make(leftXForSamplingSize(samplingSize), topYForSamplingSize(samplingSize));
 }
 
 + (GLKVector2)topRightForSamplingSize:(CGSize)samplingSize {
-    NSAssert((samplingSize.width <= 1 || samplingSize.height <= 1 ), @"samplingSize should be less than 1.0");
+    NSAssert((samplingSize.width <= 1 && samplingSize.height <= 1 ), @"samplingSize should be less than 1.0");
     return GLKVector2Make(rightXForSamplingSize(samplingSize), topYForSamplingSize(samplingSize));
 }
 
 + (GLKVector2)bottomLeftForSamplingSize:(CGSize)samplingSize {
-    NSAssert((samplingSize.width <= 1 || samplingSize.height <= 1 ), @"samplingSize should be less than 1.0");
+    NSAssert((samplingSize.width <= 1 && samplingSize.height <= 1 ), @"samplingSize should be less than 1.0");
     return GLKVector2Make(leftXForSamplingSize(samplingSize), bottomYForSamplingSize(samplingSize));
 }
 
 + (GLKVector2)bottomRightForSamplingSize:(CGSize)samplingSize {
-    NSAssert((samplingSize.width <= 1 || samplingSize.height <= 1 ), @"samplingSize should be less than 1.0");
+    NSAssert((samplingSize.width <= 1 && samplingSize.height <= 1 ), @"samplingSize should be less than 1.0");
     return GLKVector2Make(rightXForSamplingSize(samplingSize), bottomYForSamplingSize(samplingSize));
 }
 
