268e54096d7e987c930ba76e4635069a4b8b4058
billthaslu <billthaslu@tencent.com>
2022-03-02 16:01:35 +0800

    添加demo5

---
+++ Demo_OpenGLES_5/Demo_OpenGLES_5/DemoListXXXViewController.h
@@ -0,0 +1,16 @@
+//
+//  DemoListXXXViewController.h
+//  Demo_OpenGLES_5
+//
+//  Created by billthaslu on 2022/3/2.
+//
+
+#import "LXMDemoListViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DemoListXXXViewController : LXMDemoListViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo_OpenGLES_5/Demo_OpenGLES_5/DemoListXXXViewController.m
@@ -0,0 +1,28 @@
+//
+//  DemoListXXXViewController.m
+//  Demo_OpenGLES_5
+//
+//  Created by billthaslu on 2022/3/2.
+//
+
+#import "DemoListXXXViewController.h"
+
+@interface DemoListXXXViewController ()
+
+@end
+
+@implementation DemoListXXXViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"DemoListXXX";
+    
+    self.dataArray = @[
+        [LXMDemoEntranceModel entranceModelWithName:@""],
+        
+    ];
+    
+}
+
+@end
